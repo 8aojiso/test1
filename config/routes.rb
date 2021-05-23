2397874622abc415950302a5dfa2772aa080533e
yamashita <yasaize@yahoo.co.jp>
2021-05-23 12:41:20 +0900

    git rm --chached -r vendor/bundle

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get "tasks" => "tasks#index"
-  get "tasks/new" => "tasks#new"
-  post "tasks/create" =>  "tasks#create"
-  delete "tasks/:id" => "tasks#destroy"
-end
